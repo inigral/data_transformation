8ba378bb14276b93dcfd259a4b932fdb9983ce4b
Chris Barton <cbarton@inigral.com>
2011-09-12 12:02:19 -0700

    Fixing specs and updating version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 require 'rspec'
 
 require 'data_transformation'
-require 'generators/data_transformation/data_transformation_generator'
+require 'generators/transform/transform_generator'
 
 Dir[File.expand_path(File.dirname(__FILE__) + '/db')+"/*/*.rb"].each { |f| require f} 
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
